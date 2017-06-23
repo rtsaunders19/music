57a017551250673f5d1167f194a76182e82c4389
Robert Saunders <rtsaunders19@gmail.com>
2017-06-22 23:10:18 -0500

    album list

---
+++ music/src/components/AlbumList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList')
+  }
+
+  render() {
+  return (
+    <View>
+      <Text>Albums!!!!</Text>
+    </View>
+    );
+  }
+}
+export default AlbumList;
