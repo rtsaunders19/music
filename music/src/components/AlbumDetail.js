de95b9e70eb82f653169aad038b8f4c0e331ad51
Robert Saunders <rtsaunders19@gmail.com>
2017-07-06 10:56:35 -0500

    making components

--- music/src/components/AlbumDetail.js
+++ music/src/components/AlbumDetail.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const AlbumDetail = (props) => {
+  return (
+    <View>
+      <Text>{props.album.title}</Text>
+    </View>
+  );
+};
 
-}
 
 export default AlbumDetail;
--- music/src/components/AlbumList.js
+++ music/src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -15,7 +15,7 @@ class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album =>
-      <Text key={album.title}>{album.title}</Text>);
+      <Text key={album.title} album={album}></Text>);
   }
 
   render() {
