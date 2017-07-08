374bd8e44e487feec3944c956c127dc76f164de1
Robert Saunders <rtsaunders19@gmail.com>
2017-07-07 21:10:56 -0500

    more music

--- music/src/components/Button.js
+++ music/src/components/Button.js
@@ -3,10 +3,31 @@ import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
   return (
-    <TouchableOpacity>
-      <Text>Click Me</Text>
+    <TouchableOpacity onPress={() => console.log('pressed!')} style={styles.buttonStyle}>
+      <Text style={styles.textStyle}>Click Me</Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
 };
 
 export default Button;
