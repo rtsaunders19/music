b64edcdf35e11833f2adbd1b42c75834bb9413b3
Robert Saunders <rtsaunders19@gmail.com>
2017-07-11 16:50:58 -0500

    form finished

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-//not functional component so must run render() function then return
+componentWillMount() {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAN1ZFsDhAjwcPa4rKHHSNGq6z2S4e42Ag',
+    authDomain: 'authentication-e67a6.firebaseapp.com',
+    databaseURL: 'https://authentication-e67a6.firebaseio.com',
+    projectId: 'authentication-e67a6',
+    storageBucket: 'authentication-e67a6.appspot.com',
+    messagingSenderId: '725537328237'
+  });
+}
+
   render() {
     return (
       <View>
         <Header headerText={'Authentication'} />
-        <Text>An App</Text>
+        <LoginForm />
       </View>
     );
   }
